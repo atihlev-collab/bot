e72529ed67d5043e4d82c2cb04d985c93c2e0ce1
atihlev-collab <atihlev@gmail.com>
2026-05-18 19:28:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -682,41 +682,42 @@ def analyze_match(match):
         home_attacks + away_attacks
     )
 
- # =====================================================
-# MARKET
-# =====================================================
+    # =====================================================
+    # MARKET
+    # =====================================================
 
-if (
+    if (
 
-    total_goals <= 2
-    and total_shots_on >= 6
-    and total_attacks >= 28
-    and best_xg >= 1.3
-    and minute >= 35
-    and dominance < 12
+        total_goals <= 2
+        and total_shots_on >= 6
+        and total_attacks >= 28
+        and best_xg >= 1.3
+        and minute >= 35
+        and dominance < 12
 
-):
+    ):
 
-    market = "OVER 1.5 LIVE"
-
-else:
-
-           if dominance < 8:
-            return
-
-    if home_pressure > away_pressure:
-
-        market = (
-            f"NEXT GOAL HOME "
-            f"({match['teams']['home']['name']})"
-        )
+        market = "OVER 1.5 LIVE"
 
     else:
 
-        market = (
-            f"NEXT GOAL AWAY "
-            f"({match['teams']['away']['name']})"
-        )
+        if dominance < 8:
+            return
+
+        if home_pressure > away_pressure:
+
+            market = (
+                f"NEXT GOAL HOME "
+                f"({match['teams']['home']['name']})"
+            )
+
+        else:
+
+            market = (
+                f"NEXT GOAL AWAY "
+                f"({match['teams']['away']['name']})"
+            )
+
     # =====================================================
     # CONFIDENCE
     # =====================================================
