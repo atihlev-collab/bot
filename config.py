3a27d9a58565d02de7027a286768d8c8042c6ba4
atihlev-collab <atihlev@gmail.com>
2026-05-21 21:56:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 
 BOT_TOKEN = "8847822220:AAEX_4Pvj0HmUhiQbMp3z6VFkPR71IR7oJo"
-810c89c2b68279ba2bc6f6ba427bf6b9
+API_KEY = "810c89c2b68279ba2bc6f6ba427bf6b9"
 CHAT_ID = "@rangel_radar_pro"
+
  
 
 
