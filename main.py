c9b4da05ec00f76aadfc819efefca234389b3521
atihlev-collab <atihlev@gmail.com>
2026-05-22 20:40:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -255,8 +255,7 @@ def prematch_expert_runner():
 if __name__ == "__main__":
     init_database()
     print("🧠 Зареждане на Random Forest моделите при start...")
-    load_model("ml_btts.pkl")
-    load_model("ml_over.pkl")
+    load_model()
     send_telegram("🚀 БОТЪТ СТАРТИРА УСПЕШНО И Е ОНЛАЙН!")
     t1 = threading.Thread(target=live_analysis_runner)
     t2 = threading.Thread(target=prematch_expert_runner)
