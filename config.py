93842c66c0199d2735a70e3d8329fc38552a560c
atihlev-collab <atihlev@gmail.com>
2026-06-03 00:07:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-BOT_TOKEN = "8847822220:AAEX_4Pvj0HmUhiQbMp3z6VFkPR71IR7oJo"
+BOT_TOKEN = "8847822220:AAHhv-ZJulQFA_ZvHUBbfGh1wT9rj8sKiRo"
 API_KEY = "810c89c2b68279ba2bc6f6ba427bf6b9"
 CHAT_ID = "@rangel_radar_pro"
 # force restart
