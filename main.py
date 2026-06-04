267b46ed93cb804667bd5f9ea18e7aeb23a78c55
atihlev-collab <atihlev@gmail.com>
2026-06-04 21:01:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3741,11 +3741,30 @@ async def prematch_loop():
 
                     )
 
-                    confidence = 58 + score
+            confidence = 58 + score                
 
-                    over25_prob = 0
-                    btts_prob = 0
+            home_id = m["teams"]["home"]["id"]     
 
+            away_id = m["teams"]["away"]["id"]     
+
+            home_form = get_team_form(home_id)     
+
+            away_form = get_team_form(away_id)     
+
+            if not home_form:                       
+                continue                               
+
+            if not away_form:                      
+                continue                             
+
+            poisson_data = poisson_probability(     
+                home_form["avg_scored"],              
+                away_form["avg_scored"]               
+            )                                       
+
+            over25_prob = poisson_data["over25"]  
+
+            btts_prob = poisson_data["btts"]        
                     # =================================================
                     # FAIR ODDS
                     # =================================================
