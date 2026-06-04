90546c2a85a8b1817640b0f298b76050d61400e5
atihlev-collab <atihlev@gmail.com>
2026-06-05 00:08:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3721,9 +3721,8 @@ async def prematch_loop():
                     )                                        
 
                     over25_prob = poisson_data["over25"]     
-
                     btts_prob = poisson_data["btts"]     
-
+                                           ← 20
                     if market == "⚽ OVER 2.5 GOALS":                            
                         if over25_prob < 55:
                             continue                                             
