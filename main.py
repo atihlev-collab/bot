596a71ccbf043db03778cdc8c1506ba7c3318296
atihlev-collab <atihlev@gmail.com>
2026-06-06 18:47:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6641,9 +6641,9 @@ async def prematch_loop():
 
                     save_prematch(key)
 
-                import traceback
+                    import traceback
 
-                except Exception as e:
+                    except Exception as e:
 
                     print(
                         "PREMATCH MATCH ERROR:",
