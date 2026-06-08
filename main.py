c9d63e3b82f51f0bb0edab9f18bcb1039351324c
atihlev-collab <atihlev@gmail.com>
2026-06-09 00:23:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -408,8 +408,8 @@ def analyze_live_match(match):
 
 for word in blocked:
 
-      if word in text:
-          return None
+     if word in text:
+         return None
 
 fixture_id = match["fixture"]["id"]
 
