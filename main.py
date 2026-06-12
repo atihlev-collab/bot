c968ae8565643f4eacbd5ae253d3bd749ce50051
atihlev-collab <atihlev@gmail.com>
2026-06-12 16:06:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1699,6 +1699,13 @@ def analyze_prematch_match(match):
         if not match_odds:
             return None
 
+       if (
+           match_odds[0] is None
+           or
+           match_odds[2] is None
+       ):
+           return None
+
         home_drop = False
         away_drop = False
 
