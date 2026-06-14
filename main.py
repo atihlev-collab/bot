d5b17b29ce625715154f1265fe3f24881ded044d
atihlev-collab <atihlev@gmail.com>
2026-06-14 15:04:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -935,17 +935,17 @@ def analyze_live_match(match):
 
            home_pressure += 12
 
-      elif home_xg >= 1.0:
+       elif home_xg >= 1.0:
 
            home_pressure += 8
 
-      if away_xg >= 1.5:
+       if away_xg >= 1.5:
 
           away_pressure += 12
 
-      elif away_xg >= 1.0:
+       elif away_xg >= 1.0:
 
-         away_pressure += 8
+          away_pressure += 8
 
         if away_red > home_red:
 
