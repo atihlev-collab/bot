fe163d0623567126aa4bf24c8cf2463be4014ac5
atihlev-collab <atihlev@gmail.com>
2026-06-19 22:07:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -395,13 +395,7 @@ def get_match_odds(fixture_id):
 
                         away_odd = float(
                             value["odd"]
-                        )
-                        
-            send_telegram(            
-
-                f"BET={bet.get('name')} VALUES={bet.get('values')}"   
-
-            )                        
+                        )             
                
 
             print(
