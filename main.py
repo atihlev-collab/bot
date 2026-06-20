1315e9b9405ff542e35cbbbbac9290ba2a341297
atihlev-collab <atihlev@gmail.com>
2026-06-20 16:06:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2788,7 +2788,26 @@ def analyze_prematch_match(match):
                     home_super_value = True    
                 
                     home_score += 8            
-                           
+                
+                    signals.append(            
+                
+                        (                       
+                
+                            "💰 VALUE HOME",    
+                
+                            90,                 
+                
+                            round(             
+                
+                                edge,          
+                
+                                1              
+                
+                            ),                  
+                
+                        )                       
+                
+                    )                           
                 
                 elif edge >= 10:                
                 
@@ -4253,6 +4272,11 @@ if __name__ == "__main__":
         live_loop()
 
         time.sleep(300)
+
+
+
+
+
 
 
 
