73bbbb6765ed3a851bd4b26526dc8e01b7ce4f32
atihlev-collab <atihlev@gmail.com>
2026-08-08 15:27:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6119,13 +6119,13 @@ def analyze_prematch_match(match):
         if (
             not goal_match                       
             and                                
-            home_score >= 78
+            home_score >= 80
             and
             away_form["clean_sheet_pct"] <= 35           
             and
             home_odds_ok
             and
-            home_form["unbeaten_pct"] >= 75
+            home_form["unbeaten_pct"] >= 76
             and
             home_form["wins"] >= 4 
             and                         
@@ -6177,7 +6177,7 @@ def analyze_prematch_match(match):
             and
             away_form["recent_avg_conceded"] >= 0.80      
             and
-            home_probability >= 84
+            home_probability >= 86
             and
             home_balance_ok 
             and
