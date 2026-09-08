0c5b90d3e712651f1c49d2ac7264d7a3fb074944
atihlev-collab <atihlev@gmail.com>
2026-09-08 22:56:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26493,7 +26493,7 @@ def main_loop():
                 datetime.now(TIMEZONE).strftime('%H:%M:%S'),
                 'LIVE SCAN'
             )
-            sent=run_live_scan()
+            sent=_final_live_scan()
             print('LIVE SIGNALS SENT:',sent)
             LAST_LIVE_SCAN=now
 
