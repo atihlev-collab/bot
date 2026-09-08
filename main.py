ccfed93de675ea3d14a1aaab82c78749176476e6
atihlev-collab <atihlev@gmail.com>
2026-09-08 18:50:25 +0300

    Change pre-match time check to 18:52 and 21:00

--- main.py
+++ main.py
@@ -26545,7 +26545,7 @@ current_hour = current_time.hour
 current_minute = current_time.minute
 
 # PREMATCH — само в 11:00 и 21:00 BG
-if current_hour in (11, 21) and current_minute == 0:
+if current_hour in (18, 21) and current_minute == 52:
     print(
         current_time.strftime("%H:%M:%S"),
         "PREMATCH DAILY SCAN"
