157fefb80e4670149a9725b986b1780c0316e98a
atihlev-collab <atihlev@gmail.com>
2026-09-14 20:33:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = "старият ти token"
-API_KEY = "старият API-Football key"
+API_KEY = "dae2b08f-3b85-4af5-b53d-305c6db511a5"
 CHAT_ID = "@rangel_radar_pro"
 
 HIGHLIGHTLY_API_KEY = os.getenv("HIGHLIGHTLY_API_KEY")
