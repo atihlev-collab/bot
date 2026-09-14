956a84ce2770846ffcf05c90b988337e6a6bdc68
atihlev-collab <atihlev@gmail.com>
2026-09-14 23:29:20 +0300

    Update API_KEY assignment and remove old key

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 
 BOT_TOKEN = "8847822220:AAHhv-ZJulQFA_ZvHUBbfGh1wT9rj8sKiRo"
-API_KEY = "dae2b08f-3b85-4af5-b53d-305c6db511a5"
 CHAT_ID = "@rangel_radar_pro"
 
 HIGHLIGHTLY_API_KEY = os.getenv("HIGHLIGHTLY_API_KEY")
+
+# Старият API-Football ключ вече НЕ се използва
+API_KEY = HIGHLIGHTLY_API_KEY
 
 # force restart
 
